1c6e33514d7811368fc0edacfce3eebd135aa7cd
Hanna Yehoshyna <hanna.yehoshyna1982@gmail.com>
2025-01-16 21:05:40 -0700

    js-intro1

---
+++ JS/script1.js
@@ -0,0 +1,30 @@
+// function greetings()
+// {
+//     console.log(makeGreet());
+// }
+
+// function makeGreet()
+// {
+//     return 'Rise & Shine!';    
+// }
+
+// greetings();
+
+// for (var i = 0; i < 3; i++) {
+//     var name = "Hanna";
+// }
+// console.log(i);  //3, "i" is visible after loop, it's a global variable
+// console.log(name); //"name" "i" is visible after loop, it's a global variable
+
+// var name ="Hanna"
+// var name ="NotHanna" //it doesn't trigger an error
+// alert(name); //"NotHanna" 
+
+// alert(myprof);
+// var myprof="Hanna"
+
+// const MY_PROF="Hanna";
+// myProf="NotHanna"; // error, can't reassign Hanna!
+
+let myProf="Hanna"
+console.log(`Hi from ${myProf}`)
